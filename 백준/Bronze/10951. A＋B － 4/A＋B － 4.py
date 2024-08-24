34cb5e039a79702ad830856a12dc1965b7e5446d
Gayoung Lee <153724719+gaebonglee@users.noreply.github.com>
2024-08-25 04:41:08 +0900

    [Bronze V] Title: A+B - 4, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -1,5 +1,6 @@
-import sys
-
-for line in sys.stdin:
-    A, B = map(int, line.split())
-    print(A + B)
+while True:
+    try :
+        A, B = map(int, input().split())
+        print(A+B)
+    except:
+        break
