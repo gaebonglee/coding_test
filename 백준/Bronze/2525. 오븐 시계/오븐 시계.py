1efc9113b7bcbf85136504d1f9bf87ef1939aa61
Gayoung Lee <153724719+gaebonglee@users.noreply.github.com>
2024-09-22 20:54:27 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,10 +1,11 @@
 A, B = map(int, input().split())
 C = int(input())
 
-total_minutes = A * 60 + B
-total_minutes += C
+total_minutes = B + C
 
-new_A = (total_minutes // 60) % 24
-new_B = total_minutes % 60
+A += total_minutes // 60  
+B = total_minutes % 60 
 
-print(new_A, new_B)
+A %= 24
+
+print(A, B)
