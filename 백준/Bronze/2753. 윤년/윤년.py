4c795334c7f45f97faca630a20806fb3c884c0c5
Gayoung Lee <153724719+gaebonglee@users.noreply.github.com>
2024-09-22 20:04:45 +0900

    [Bronze V] Title: 윤년, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,9 +1,5 @@
-year = int(input())
-
-1<=year<=4000
-
-
-if (year%4==0 and year%100 !=0)or (year%400==0):
-    print(1)
-else :
-    print(0)
+year =int(input())
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("1")
+else:
+    print("0")
