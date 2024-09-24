b0def9741f7de8ce767bf3d1ce6d12031e1a5c4b
Gayoung Lee <153724719+gaebonglee@users.noreply.github.com>
2024-09-24 19:48:41 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,6 +1,6 @@
-numbers = [int(input()) for _ in range(9)] 
-maxNum = max(numbers)  
-index = numbers.index(maxNum) + 1
+N = [int(input())for _ in range(9)]
+maxNum = max(N)
+index = N.index(maxNum)+ 1
 
-print(maxNum)  # 최댓값 출력
+print(maxNum)
 print(index)
