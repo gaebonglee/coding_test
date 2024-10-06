560a5efa9ca58943ca7401af448005ae0be6848c
Gayoung Lee <153724719+gaebonglee@users.noreply.github.com>
2024-10-07 00:23:59 +0900

    [Bronze III] Title: 세 수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.py
+++ 백준/Bronze/10817. 세 수/세 수.py
@@ -1,13 +1,9 @@
 def second_largest(a,b,c):
-    if (a >= b and a <= c) or (a >= c and a <= b):
-        return a
-    elif (b >= a and b <= c) or (b >= c and b <= a):
-        return b
-    else:
-        return c
+    numbers = [a, b, c]
+    numbers.sort()
+    return numbers[1]
 
 a, b, c = map(int, input().split())
 
-
-result = second_largest(a,b,c)
+result = second_largest(a, b, c)
 print(result)
